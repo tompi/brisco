566d9bb7bccd36686564a561f0cbf92909e87923
Thomas Haukland <thomas.haukland@gmail.com>
2012-09-16 11:35:10 -0400

    facebook sso

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ var config = {
     GOOGLE_CLIENT_SECRET: "-Hiq3gWKdcdR2812CbEZz3rA",
     GOOGLE_CALLBACK_URL: LOCAL_URL + "/oauth2callback",
     
-    FACEBOOK_APP_ID: "jalla",
-    FACEBOOK_APP_SECRET: "julla",
-    FACEBOOK_CALLBACK_URL: LOCAL_URL + "/auth/facebook/callback",
+    FACEBOOK_APP_ID: "467889913242015",
+    FACEBOOK_APP_SECRET: "6f75a105bfcceeca498c1b541a2b0d22",
+    FACEBOOK_CALLBACK_URL: LOCAL_URL + "/auth/facebook/callback", 
     
     COOKIE_SECRET: "EAEW62JRUFS70+TRWQ1232"
 };

424ee26d5d1c3a279c025fac14ccaddfe2779a9a
Thomas Haukland <thomas.haukland@gmail.com>
2012-09-20 17:32:42 -0400

    login

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ var app = express();
 app.configure(function() {
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
-    app.engine('ejs',require('ejs-locals'));
+    app.engine('ejs', require('ejs-locals'));
     app.locals._layoutFile = 'layout.ejs';
     app.set('view engine', 'ejs');
     app.use(express.favicon());
@@ -93,17 +93,17 @@ app.configure(function() {
     app.use(app.router);
     app.use(require('stylus').middleware(__dirname + '/public'));
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(function(req, res, next) {
+        res.locals.user = req.session.user;
+        next();
+    });
 });
 
 app.configure('development', function() {
     app.use(express.errorHandler());
 });
 
-app.get('/account', ensureAuthenticated, function(req, res) {
-    res.render('account', {
-        user: req.user
-    });
-});
+
 
 app.get('/account-json', ensureAuthenticated, function(req, res) {
     res.json(req.user);
@@ -138,17 +138,24 @@ function(req, res) {
 
 
 app.get('/auth/linkedin',
-  passport.authenticate('linkedin'),
-  function(req, res){
+passport.authenticate('linkedin'),
+
+function(req, res) {
     // The request will be redirected to LinkedIn for authentication, so this
     // function will not be called.
-  });
+});
 
-app.get('/auth/linkedin/callback',   passport.authenticate('linkedin', { failureRedirect: '/login' }),  routes.loggedin);
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+    failureRedirect: '/login'
+}), routes.loggedin);
 
-app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/login'}),routes.loggedin);
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/login'
+}), routes.loggedin);
 
-app.get('/oauth2callback', passport.authenticate('google', {failureRedirect: '/login'}),routes.loggedin);
+app.get('/oauth2callback', passport.authenticate('google', {
+    failureRedirect: '/login'
+}), routes.loggedin);
 
 app.get('/logout', function(req, res) {
     req.logout();
@@ -156,16 +163,12 @@ app.get('/logout', function(req, res) {
 });
 
 app.get('/', routes.index);
+app.get('/start', routes.start);
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
 });
 
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,6 @@ exports.index = function(req, res){
   res.render('index', { title: 'DupliScore' });
 };
 
-exports.loggedin = function(req, res) {
-     res.redirect('/account-json');
-  };
+exports.start = function(req, res) {
+    res.render('start');
+};
